2ef3e42bc955f279f55e801bd7cf7c65c81e4c56
EbubeCode <chukwuma258@gmail.com>
2022-09-18 08:59:37 -0700

    0x0A task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # install flask
 node default {
   package {'flask':
-    ensure   => '2.1.0'
-    provider => 'pip',
+    ensure   => '2.1.0',
+    name     => 'flask',
+    provider => 'pip3',
   }
 }
