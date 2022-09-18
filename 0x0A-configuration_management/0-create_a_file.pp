f7d0b4ddba19c020cedef43e94901989896b7cf3
EbubeCode <chukwuma258@gmail.com>
2022-09-18 15:21:31 +0100

    0x0A task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,12 @@
 # create a file
-file { '/tmp/school':
-	mode => 0744,
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+node default {
+  file { '/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+  }
 }
+
 
