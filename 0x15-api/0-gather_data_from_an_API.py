d2b6d5a631c846c131bc0219044384f4b389627e
EbubeCode <chukwuma258@gmail.com>
2022-10-18 04:06:47 +0100

    0x15 task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# get employees
+import sys
+import requests
+
+if __name__ == "__main__" and len(sys.argv) >= 2:
+    id = sys.argv[1]
+    req = f'https://jsonplaceholder.typicode.com/users/{id}'
+    r = requests.get(req)
+    name = r.json()['name']
+    req = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    r = requests.get(req)
+    todos = r.json()
+    no_t = len(todos)
+    no_c = 0
+    complete = ''
+    for v in todos:
+        if v['completed']:
+            if no_c != 0:
+                complete += f"\n\t {v['title']}"
+            else:
+                complete += f"\t {v['title']}"
+            no_c += 1
+    print(f'Employee {name} is done with tasks ({no_c}/{no_t}):')
+    print(complete)
