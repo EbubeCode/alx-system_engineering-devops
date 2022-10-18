121d758b97309fc08a479390a89fda0e708b7bf7
EbubeCode <chukwuma258@gmail.com>
2022-10-18 04:19:37 +0100

    0x15 task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        exit(1)
     id = sys.argv[1]
     req = f'https://jsonplaceholder.typicode.com/users/{id}'
     r = requests.get(req)
